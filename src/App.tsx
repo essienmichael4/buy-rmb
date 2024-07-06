1fbd9233175a528243fb0ec5d45cafbb12ebbfda
Code_Junior <essienmichael4@gmail.com>
2024-07-06 20:00:20 +0000

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+         {/* <Route element={< />}> */}
+        //     <Route path=''>
+        //       {/* <Route path="dashboard" element={<Dashboard/>} /> */}
+        //     </Route>
+         {/* </Route> */}
+      </Routes>
     </>
   )
 }
